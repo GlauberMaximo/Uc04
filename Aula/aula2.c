f8d91076379dddb13b263712252fdce05f6c0c63
GlauberMaximo <155695736+GlauberMaximo@users.noreply.github.com>
2025-06-05 17:07:58 -0300

    Add files via upload

---
+++ Aula/aula2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int idade = 25;
+    float altura = 1.75;
+    char letra = '*';
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Letra: %d\n", letra);
+
+    return 0;
+}
